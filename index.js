4864ff2a3537e6bd46162cce709d1789d6630d2d
Kelsey-Hunt <kels.hunt13@gmail.com>
2022-09-09 15:24:25 -0500

    completed task 3

--- index.js
+++ index.js
@@ -46,8 +46,7 @@ function getFinals(data) {
     });
     return finalsList;
  }
-
-
+//console.log(getFinals(fifaData));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Use the higher-order function called getYears to do the following: 
@@ -55,10 +54,14 @@ Use the higher-order function called getYears to do the following:
 2. Receive a callback function as the second parameter that will take getFinals from task 2 as an argument
 3. Return an array called years containing all of the years in the getFinals data set*/
 
-function getYears(/* code here */) {
-    /* code here */
+function getYears(data, cb) {
+    const finalsData = cb(data);
+    const yearsList = finalsData.map((element) => {
+        return element.Year;
+    });
+    return yearsList;
 }
-
+console.log(getYears(fifaData, getFinals));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
